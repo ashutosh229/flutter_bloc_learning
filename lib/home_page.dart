1352f8f35509c09b9a4b1f6d93b23424ed57eaa7
ashutosh229 <akumarjha875@gmail.com>
2025-03-12 18:58:42 +0530

    added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,54 +3,51 @@ import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title})
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
   Widget build(BuildContext context) {
     final counterCubit = BlocProvider.of<CounterCubit>(context);
-    return BlocBuilder<CounterCubit, int>(
-        bloc: counterCubit,
-        builder: (context, counter) {
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-              title: Text(title),
+    final counter = counterCubit.state;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
-                    '$counter',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                ],
-              ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-            floatingActionButton: Column(
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    counterCubit.increment();
-                  },
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    counterCubit.decrement();
-                  },
-                  tooltip: 'Decrement',
-                  child: const Icon(Icons.minimize),
-                ),
-              ],
-            ),
-          );
-        });
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterCubit.increment();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              counterCubit.decrement();
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ),
+    );
   }
 }
