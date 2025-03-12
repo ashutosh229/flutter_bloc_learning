ac8fb9b7b24b96048024d663d05d2ce56427198e
ashutosh229 <akumarjha875@gmail.com>
2025-03-12 19:02:37 +0530

    aded

--- lib/inc_dec_page.dart
+++ lib/inc_dec_page.dart
@@ -1,29 +1,35 @@
 import 'package:bloc_pattern/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class IncDecPage extends StatelessWidget {
   const IncDecPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CounterCubit, int>(bloc: ,builder: (context) {
-      return Scaffold(
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () {},
-              tooltip: "Increment",
-              child: const Icon(Icons.add),
-            ),
-            FloatingActionButton(
-              onPressed: () {},
-              tooltip: "Decrement",
-              child: Icon(Icons.minimize),
-            )
-          ],
-        ),
-      );
-    });
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
+    final incrementFunc = counterCubit.increment();
+    final decrementFunc = counterCubit.decrement();
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              incrementFunc;
+            },
+            tooltip: "Increment",
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              decrementFunc;
+            },
+            tooltip: "Decrement",
+            child: Icon(Icons.minimize),
+          )
+        ],
+      ),
+    );
   }
 }
